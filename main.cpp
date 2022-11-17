108ac5c03b13a1c11b8ca17e8f041d45dae452dd
3487 Red Pride Robotics <team3487@gmail.com>
2022-11-17 09:32:12 -0500

    get distance from pose and publish to NT

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@
 #include "apriltag/common/zarray.h"
 #include "apriltag/apriltag.h"
 #include "apriltag/tag36h11.h"
+#include "apriltag/apriltag_pose.h"
 
 #include "opencv4/opencv2/core/mat.hpp"
 #include "opencv4/opencv2/calib3d/calib3d.hpp"
@@ -270,51 +271,57 @@ class MyPipeline : public frc::VisionPipeline {
  public:
       apriltag_detector_t *td = apriltag_detector_create();
       apriltag_family_t *tf = tag36h11_create();
-      nt::NetworkTableEntry detectionCenter;
-      nt::NetworkTableEntry detectionBounds;
+      nt::NetworkTableEntry detectionDistance;
       double cameraArray[3][3] = {{1430.24403,0.0,645.226606},{0.0,1415.29681,487.429715},{0.0,0.0,1.0}};
       double newCameraArray[3][3] = {1413.22522,0.0,645.461355,0.0,1407.51892,488.005117,0.0,0.0,1.0};
       double dist[5] = {-0.0322087,-0.01251858,0.00189451,0.00087394,0.37527268};
       cv::Mat cameraMatrix = cv::Mat(3, 3, CV_32FC1, &cameraArray);
       cv::Mat newCameraMatrix = cv::Mat(3, 3, CV_32FC1, &newCameraArray);
       cv::Mat distMatrix = cv::Mat(1,5,CV_32FC1, &dist);
-      int roi[4] = {4,2,1272,954};
-      cv::Mat roiMatrix = cv::Mat(1,4,CV_32FC1,&roi);
-      int x = roi[0];
-      int y = roi[1];
-      int w = roi[2];
-      int h = roi[3];
+      int x = 4;
+      int y = 2;
+      int w = 1272;
+      int h = 954;
+      apriltag_detection_info_t info;
+      double tagsize = 0.156;
+      double target_distance_from_tag = 23/100; // radius of basketball hoop (cm * m/cm)
   
   MyPipeline(void){
     apriltag_detector_add_family(td, tf);
     auto inst = nt::NetworkTableInstance::GetDefault();
     auto table = inst.GetTable("datatable");
-    detectionCenter = table->GetEntry("Detection Center");
-    detectionBounds = table->GetEntry("Detection Bounds");
+    detectionDistance = table->GetEntry("Detection Distance");
+    info.tagsize = tagsize;
+    info.fx = cameraArray[0][0];
+    info.fy = cameraArray[1][1];
+    info.cx = cameraArray[0][2];
+    info.cy = cameraArray[1][2];
   }
 
   void Process(cv::Mat& mat) override {
     //TODO: refractor (probably don't need to create and destroy the detector every frame)
-
-    //undistort image
-    cv::Mat correct_mat;
-    cv::undistort(mat,correct_mat,cameraMatrix,distMatrix,newCameraMatrix);
-    image_u8_t img_header = { .width = correct_mat.cols,
-    .height = correct_mat.rows,
-    .stride = correct_mat.cols,
-    .buf = correct_mat.data
+    image_u8_t img_header = { .width = mat.cols,
+    .height = mat.rows,
+    .stride = mat.cols,
+    .buf = mat.data
     };
     zarray_t *detections = apriltag_detector_detect(td, &img_header);
     for (int i = 0; i < zarray_size(detections); i++) {
     apriltag_detection_t *det;
     zarray_get(detections, i, &det);
 
-    // Do stuff with detections here.
-    // I'm stuff
+    info.det = det;
 
-    detectionCenter.SetDoubleArray(det->c);
-    detectionBounds.SetDoubleArray(det->p[0]);
+    apriltag_pose_t pose;
+    estimate_tag_pose(&info, &pose);
+
+
+    matd_t* detection_coordinates = matd_multiply(matd_multiply(matd_transpose(pose.R), matd_create_scalar(-1)),pose.t);
+
     
+     detectionDistance.SetDouble(sqrt((pow(matd_get(detection_coordinates,0,2)+target_distance_from_tag,2))+pow(matd_get(detection_coordinates,0,0),2))); 
+    // Do stuff with detections here.
+    // I'm stuff    
 }
   }
 };
