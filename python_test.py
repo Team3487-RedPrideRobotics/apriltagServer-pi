15b46ac30bc01a7f883d7b602d59a5afe23edbb7
SilicaSandwhich <silicasandwhich@github.com>
2022-11-28 22:17:36 -0500

    preliminary distance measurement and angle finding

--- python_test.py
+++ python_test.py
@@ -1,3 +1,5 @@
+from math import asin, atan, atan2, sqrt
+import math
 import cv2
 import numpy as np
 from dt_apriltags import Detector
@@ -15,8 +17,8 @@ at_detector = Detector(searchpath=['apriltags'],
 
 camera_matrix = [[985.51615849,0.,657.87528672],[0.,984.87908342,354.97674327],[  0.,0.,1.]]
 camera_params = [camera_matrix[0][0],camera_matrix[1][1],camera_matrix[0][2],camera_matrix[1][2]]
-world_point_pose = np.zeros(3)
-tag_size = 6*2.54/100 # in * cm/in * m/cm
+tag_size = 6*2.54/100 * 1.216# in * cm/in * m/cm
+dist_from_obj = 55 * 2.54/100 # in * cm/in * m/cm
 
 def integerize_tuple(tup):
     output = []
@@ -32,6 +34,19 @@ def draw_tag(det, frame):
     cv2.line(frame,integerize_tuple(corners[1]),integerize_tuple(corners[2]),(0,255,0),3)
     cv2.line(frame,integerize_tuple(corners[2]),integerize_tuple(corners[3]),(0,0,255),3)
     cv2.line(frame,integerize_tuple(corners[3]),integerize_tuple(corners[0]),(255,0,255),3)
+
+    R = det.pose_R.transpose()
+    yaw = asin(R[2][0]) # from parallel to tag facing direction
+    camera_pose = -np.matrix(R).T * np.matrix(det.pose_t) # [[X],[Y],[Z]]
+    distance = sqrt(camera_pose[0][0] ** 2 + (camera_pose[2][0]+dist_from_obj) ** 2)
+    required_angle = atan(camera_pose[0][0]/(camera_pose[2][0]+dist_from_obj))
+    if abs(camera_pose[2][0]) < dist_from_obj:
+        required_angle += math.pi/2
+    delta_angle = required_angle - yaw
+
+    cv2.putText(frame,str(distance),integerize_tuple(corners[2]),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0))
+    cv2.putText(frame,str(delta_angle),integerize_tuple(corners[0]),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255))
+
 
 
 
@@ -51,8 +66,9 @@ while(cap.isOpened()):
         for tag in tags:
             if(tag.tag_id != 0 or tag.pose_err > 1e-05):
                 continue
-            print(tag.pose_err)
-            #print(np.array(tag.pose_t).flatten(),np.array(tag.pose_R).flatten())
+            
+            
+
             draw_tag(tag,frame)
         cv2.imshow('Frame',frame)
         # Press Q on keyboard to  exit
