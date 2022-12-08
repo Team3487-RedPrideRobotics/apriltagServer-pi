c09b2106e6ab2871991f95651ca5bef327c13c3a
3487 Red Pride Robotics <team3487@gmail.com>
2022-12-08 10:45:07 -0500

    new calibration

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -5,8 +5,8 @@ import glob
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(7,5,0)
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+objp = np.zeros((4*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:4,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -16,11 +16,16 @@ for fname in images:
     img_orig = img
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,7), None)
+    ret, corners = cv.findChessboardCorners(gray, (4,6), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        cv.drawChessboardCorners(img, (4,6), corners2, ret)
+        cv.imshow('img', img)
+        if(cv.waitKey(0) == ord('q')):
+            os.remove(fname)
+            break
         imgpoints.append(corners2)
     else:
         os.remove(fname)
